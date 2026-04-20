0649ac1f913b7e965352a40966424965a6b33616
VoThiNgocHan <155296503+VoThiNgocHan@users.noreply.github.com>
2026-04-20 21:35:50 +0700

    update alu

--- work/alu/_primary.vhd
+++ work/alu/_primary.vhd
@@ -1,33 +1,10 @@
 library verilog;
 use verilog.vl_types.all;
 entity alu is
-    generic(
-        add             : vl_logic_vector(0 to 3) := (Hi0, Hi0, Hi0, Hi0);
-        sub             : vl_logic_vector(0 to 3) := (Hi0, Hi0, Hi0, Hi1);
-        \sll\           : vl_logic_vector(0 to 3) := (Hi0, Hi0, Hi1, Hi0);
-        slt             : vl_logic_vector(0 to 3) := (Hi0, Hi0, Hi1, Hi1);
-        sltu            : vl_logic_vector(0 to 3) := (Hi0, Hi1, Hi0, Hi0);
-        xxor            : vl_logic_vector(0 to 3) := (Hi0, Hi1, Hi0, Hi1);
-        \srl\           : vl_logic_vector(0 to 3) := (Hi0, Hi1, Hi1, Hi0);
-        \sra\           : vl_logic_vector(0 to 3) := (Hi0, Hi1, Hi1, Hi1);
-        oor             : vl_logic_vector(0 to 3) := (Hi1, Hi0, Hi0, Hi0);
-        aand            : vl_logic_vector(0 to 3) := (Hi1, Hi0, Hi0, Hi1)
-    );
     port(
         i_op_a          : in     vl_logic_vector(31 downto 0);
         i_op_b          : in     vl_logic_vector(31 downto 0);
         i_alu_op        : in     vl_logic_vector(3 downto 0);
         o_alu_data      : out    vl_logic_vector(31 downto 0)
     );
-    attribute mti_svvh_generic_type : integer;
-    attribute mti_svvh_generic_type of add : constant is 1;
-    attribute mti_svvh_generic_type of sub : constant is 1;
-    attribute mti_svvh_generic_type of \sll\ : constant is 1;
-    attribute mti_svvh_generic_type of slt : constant is 1;
-    attribute mti_svvh_generic_type of sltu : constant is 1;
-    attribute mti_svvh_generic_type of xxor : constant is 1;
-    attribute mti_svvh_generic_type of \srl\ : constant is 1;
-    attribute mti_svvh_generic_type of \sra\ : constant is 1;
-    attribute mti_svvh_generic_type of oor : constant is 1;
-    attribute mti_svvh_generic_type of aand : constant is 1;
 end alu;
