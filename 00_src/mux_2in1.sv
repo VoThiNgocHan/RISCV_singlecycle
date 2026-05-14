a5dc7bcca06ff01f17b9355fbc1b012704faf3ef
VoThiNgocHan <155296503+VoThiNgocHan@users.noreply.github.com>
2026-05-15 01:33:23 +0700

    remove 00_src from repo

--- 00_src/mux_2in1.sv
+++
@@ -1,9 +0,0 @@
-module mux_2in1(
-    input  logic [31:0] i_in0,
-    input  logic [31:0] i_in1,
-    input  logic        i_sel,
-    output logic [31:0] o_out
-);
- assign o_out = i_sel ? i_in1 : i_in0;
-
-endmodule
