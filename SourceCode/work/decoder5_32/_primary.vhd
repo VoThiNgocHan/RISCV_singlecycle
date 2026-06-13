0f473be40416c773e6e19344987f1ab9989a932b
VoThiNgocHan <155296503+VoThiNgocHan@users.noreply.github.com>
2026-06-13 22:04:33 +0700

    Remove ModelSim generated files

--- SourceCode/work/decoder5_32/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity decoder5_32 is
-    port(
-        \in\            : in     vl_logic_vector(4 downto 0);
-        en              : in     vl_logic;
-        y               : out    vl_logic_vector(31 downto 0)
-    );
-end decoder5_32;
--- SourceCode/work/mux32_1/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity mux32_1 is
-    port(
-        sel             : in     vl_logic_vector(4 downto 0);
-        d               : in     vl_logic_vector(31 downto 0);
-        y               : out    vl_logic
-    );
-end mux32_1;
