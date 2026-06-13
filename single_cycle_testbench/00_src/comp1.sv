9ebaef87c6291440849e73bb6f9795bd855f1140
VoThiNgocHan <155296503+VoThiNgocHan@users.noreply.github.com>
2026-06-13 21:34:09 +0700

    Remove single_cycle_testbench

--- single_cycle_testbench/00_src/comp1.sv
+++
@@ -1,9 +0,0 @@
-module comp1 (
-    input  logic A,
-    input  logic B,
-    output logic E,   // Equal
-    output logic L    // Less
-);
-    assign E = ~(A ^ B);     
-    assign L = (~A) & B;     // A < B
-endmodule
--- single_cycle_testbench/00_src/full_adder.sv
+++
@@ -1,7 +0,0 @@
-module full_adder(
-  input  logic a, b, cin,
-  output logic sum , cout
-);
-  assign sum = a ^ b ^ cin;
-  assign cout = (a & b) | (a & cin) | (b & cin);
-endmodule
